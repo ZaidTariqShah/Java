496733781ca040ad2e004f8739a1005f0dc098bb
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2023-09-14 14:28:07 +0530

    Add files via upload

---
+++ Basic_Arithmatic_Operators.java
@@ -0,0 +1,10 @@
+public class Basic_Arithmatic_Operators {
+    public static void main(String args[]) {
+        int a = 1;
+        int b = 9;
+        System.out.println("sum = "+(a + b));           // Addition
+        System.out.println("sub = "+(a - b));           // Subtraction
+        System.out.println("Mul = "+(a * b));           // Multiplication
+        System.out.println("Div = "+(a / b));           // Division
+    }
+}
